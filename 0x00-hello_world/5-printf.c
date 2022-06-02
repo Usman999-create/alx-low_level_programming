f77f6f1a1ac8511a769bcbb9671f6641e092cda8
usman yusuf <yusuf4all99@gmail.com>
2022-06-02 21:25:59 +0000

    5-printf.c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ *
+ *Description: C program that prints Using the function printf
+ *
+ * Return: 0 number
+ */
+int main(void)
+{
+		printf("with proper grammar, but the outcome is a piece of art,\n");
+			return (0);
+
+}
