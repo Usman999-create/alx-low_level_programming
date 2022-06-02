9dbd7f50d29436244eff3942c55a130b4311ebe3
usman yusuf <yusuf4all99@gmail.com>
2022-06-02 22:40:12 +0000

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,11 @@
-#include <unistd>
+#include <unistd.h>
 /**
- *main - Entry point
- *
- *Description: print a qoute using a write function
- *
- *Return: return error 1
- */
+* main - Entry point
+*
+* Description: print a qoute using a write function
+*
+* Return: return error 1
+*/
 int main(void)
 {
 char quo[] = "and that piece of art is useful\" - Dora korpar, Dora Korpar, 2015-10-19\n";
