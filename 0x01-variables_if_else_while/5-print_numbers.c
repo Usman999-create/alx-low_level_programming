53c5257b18d740f4bd033bbe9ba1a4583dc1cc90
usman yusuf <yusuf4all99@gmail.com>
2022-06-03 21:41:29 +0000

    5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: prints all the alphabets in lower case
+ *
+ * Return: return all the alphabets
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 0; n <= 10; ++n)
+	{
+		printf("%d", n);
+	}
+	printf("\n");
+	return (0);
+}
+
