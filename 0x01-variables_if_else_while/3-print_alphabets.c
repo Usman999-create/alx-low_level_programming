6262f3822d990427b1d20ab47da44fb313517b3d
usman yusuf <yusuf4all99@gmail.com>
2022-06-03 21:22:36 +0000

    3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,22 +1,22 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * Description:prints all the alphabets in lower case
+ * Description: prints all the alphabets in lower case
  *
  * Return: return all the alphabets
  */
 int main(void)
 {
-	char c;
+char c;
 
-	for (c = 'a'; c <= 'z'; ++c)
-	{
-		putchar(c);
-	}
-	for (c = 'A'; c <= 'Z'; ++c)
-	{
-		putchar(c);
-	}
-	putchar("\n");
-	return (0);
+for (c = 'a'; c <= 'z'; ++c)
+{
+putchar(c);
+}
+for (c = 'A'; c <= 'Z'; ++c)
+{
+putchar(c);
+}
+putchar('\n');
+return (0);
 }
