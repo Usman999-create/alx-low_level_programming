bbc2664ceefc6dafcbb11b2a457e50873c7e4179
usman yusuf <yusuf4all99@gmail.com>
2022-06-03 20:59:02 +0000

    1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -4,30 +4,31 @@
 /**
  * main - Entry point
  *
- * Description: program that assign a random number n each time it is executed
+ * Description: program that assigns a random number
  *
- * Return: return 1 of 3 option
+ * Return: return one of the 3 options
  */
 int main(void)
 {
 int n;
-int last_dig;
+int last_num;
 
 srand(time(0));
 n = rand() - RAND_MAX / 2;
-last_dig = n % 10;
 
-if (last_dig > 5)
+last_num = n % 10;
+
+if (last_num > 5)
 {
-printf("Last digit of %d is %d and is greater than 5\n", n, last_dig);
+printf("Last digit of %d is %d and is greater than 5\n", n, last_num);
 }
-else if (last_dig == 0)
+else if (last_num == 0)
 {
-printf("last digit of %d is %d and is 0\n", n, last_dig);
+printf("Last digit of %d is %d and is 0\n", n, last_num);
 }
 else
 {
-printf("Last digit of %d is %d is less than 6 and not 0\n", n, last_dig);
+printf("Last digit of %d is %d and is less than 6 and not 0\n", n, last_num);
 }
 return (0);
 }
