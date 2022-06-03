df1d8efbade67f3f8d4a3a3316614d15e3e5aed5
usman yusuf <yusuf4all99@gmail.com>
2022-06-03 23:59:57 +0000

    102-print_comb5.c

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,43 +6,43 @@
  */
 int main(void)
 {
-	int a = 0, b = 0, c = 0, d = 0;
-	int coma = 1;
+int a = 0, b = 0, c = 0, d = 0;
+int coma = 1;
 
-	while (a <= 9)
-	{
-		while (b <= 9)
-		{
-			while (c <= 9)
-			{
-				while (d <= 9)
-				{
-					if ((c * 10) + d > (a * 10) + b)
-					{
-						if (coma == 0)
-						{
-							putchar(',');
-							putchar(32);
-						}
-						putchar(a + '0');
-						putchar(b + '0');
-						putchar(32);
-						putchar(c + '0');
-						putchar(d + '0');
-						coma = 0;
-					}
-					++d;
-				}
-				++c;
-				d = 0;
-			}
-			++b;
-			c = 0;
-		}
-		++a;
-		b = 0;
-	}
-	putchar(10);
+while (a <= 9)
+{
+while (b <= 9)
+{
+while (c <= 9)
+{
+while (d <= 9)
+{
+if ((c * 10) + d > (a * 10) + b)
+{
+if (coma == 0)
+{
+putchar(',');
+putchar(32);
+}
+putchar(a + '0');
+putchar(b + '0');
+putchar(32);
+putchar(c + '0');
+putchar(d + '0');
+coma = 0;
+}
+++d;
+}
+++c;
+d = 0;
+}
+++b;
+c = 0;
+}
+++a;
+b = 0;
+}
+putchar(10);
 
-	return (0);
+return (0);
 }
