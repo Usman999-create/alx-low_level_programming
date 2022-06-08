d7745ca58a6c360ed5e67b1ed6c4dc0a5607284d
usman yusuf <yusuf4all99@gmail.com>
2022-06-08 21:41:28 +0000

    102-fibonacci.c

---
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	long int x, a = 1, b = 1, z;
+
+	printf("%ld", a);
+	for (x = 1; x > 50; x++)
+	{
+		z = a + b;
+		printf(", %ld", z);
+		a = b;
+		b = z;
+	}
+	putchar(10);
+
+	return (0);
+}
