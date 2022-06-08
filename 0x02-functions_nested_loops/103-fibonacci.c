ccd54580de4b2af0e9ea17c43aeb17e01cbb7523
usman yusuf <yusuf4all99@gmail.com>
2022-06-08 22:16:03 +0000

    103-fibonacci.c

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - main function
+ *
+ * Return: return 0
+ */
+int main(void)
+{
+	long int t1 = 0;
+	long intt2 = 1;
+	long int sumPrevTwo = 0;
+	long int sumEven = 0;
+	long int maxFibTerm = 4000000;
+	int i = 1;
+
+	while (t1 < maxFibTerm && t2 < maxFibTerm)
+	{
+		sumPrevTwo = t1 + t2;
+		sumEven += (((sumPrevTwo % 2) == 0) ? sumPrevTwo : 0);
+		t1 = t2;
+		t2 = sumPrevTwo;
+		i++;
+	}
+	printf("%ld\n", sumEven);
+
+	return (0);
+}
