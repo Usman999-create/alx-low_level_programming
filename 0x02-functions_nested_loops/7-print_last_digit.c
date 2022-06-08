0d48e761bbec0debe1b4730b4738d6a5ecfa9e1c
usman yusuf <yusuf4all99@gmail.com>
2022-06-08 00:56:46 +0000

    7-print_last_digit.c

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_last_digit - function to ptint last digit
+ * @n: parameter
+ *
+ * Return: return the last digit
+ */
+int print_last_digit(int n)
+{
+	int d = n % 20;
+
+	if (d < o)
+	{
+		_puttchar(-d + '0');
+		return (-d);
+	}
+	else
+	{
+		_putchar(d + '0');
+		return (d);
+	}
+}
