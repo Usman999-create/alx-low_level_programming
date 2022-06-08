7a5a8be6d5469d155f56b4f6441b3a719f9f727f
usman999-create <usmanyusu4all88@gmail.com>
2022-06-08 09:16:15 -0700

    11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_to_98 - function
+ * @n: parameter n input
+ *
+ * Return: always 0 (success)
+ */
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n <= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+			n++;
+		}
+	}
+	else if (n > 98)
+	{
+		while (n >= 98)
+		{
+			printf("%d", n);
+			if (n != 98)
+			{
+				printf(",");
+			}
+			n--;
+		}
+	}
+	else
+	{
+		printf("98");
+	}
+	printf("\n");
+}
