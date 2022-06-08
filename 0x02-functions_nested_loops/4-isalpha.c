73e44bd884af2d81e85a0bb85c2c60d32c9e6fac
usman yusuf <yusuf4all99@gmail.com>
2022-06-08 00:38:30 +0000

    4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,7 +1,8 @@
 #include "main.h"
 /**
- * int _isalpha - 'c'
+ *_isalpha - function to know if
  * the character is a letter or not
+ * @c: variable to evaluate
  *
  * Return: Get 1 if it's a letter,
  * 0 otherwise
