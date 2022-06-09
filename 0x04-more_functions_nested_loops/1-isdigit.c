04a7112bde4d1e6bc13d312fb3c1b9734d58f506
usman yusuf <yusuf4all99@gmail.com>
2022-06-09 15:04:48 +0000

    1-isdigit.c

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * _isdigit - function
+ *
+ * @c: parameter
+ *
+ * Return: return Value
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	else
+		return (0);
+}
