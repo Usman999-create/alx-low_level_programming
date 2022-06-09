a6965acc4c229234fe5fc121fd2975335050f71d
usman yusuf <yusuf4all99@gmail.com>
2022-06-09 17:34:02 +0000

    4-print_most_numbers.c

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * print_most_numbers - function
+ */
+void print_most_numbers(void)
+{
+	int n = 0;
+
+	while (n < 9)
+	{
+		if (n != 2 && n != 4)
+			_putchar(n);
+		n++;
+	}
+	_putchar('\n');
+}
