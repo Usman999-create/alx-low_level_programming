3195289bb716f23160205765939d18b2800759be
usman yusuf <yusuf4all99@gmail.com>
2022-06-13 21:03:41 +0000

    100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _atoi - function that convert string to numbers
+ * @s: parameter
+ *
+ * Return: Always 0.
+ */
+int _atoi(char *s)
+{
+	char s[] = "123";
+	int a = atoi(s);
+
+
+	printf("%d", a);
+	return (0);
+}
