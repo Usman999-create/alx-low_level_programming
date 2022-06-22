f1559975876fc2aa6d4c4e8f424c37e421439241
usman999-create <usmanyusu4all88@gmail.com>
2022-06-22 05:45:30 -0700

    6-is_prime_number.c

---
+++ 0x08-recursion/6-is_prime_number.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * is_prime - function
+ * @x: number
+ * @y: number
+ *
+ * Return: return 0 if not prime
+ * return 1 if prime
+ */
+int is_prime_number(int x, int y)
+{
+	if ((x % y) == 0 && y < x)
+		return (0);
+	if (x == 0)
+		return (1);
+	else
+		return (is_prime(x, y + 1));
+}
+/**
+ * is_prime_number - description
+ * @n: verification number
+ * Return: return 0 if prime
+ * and 1 if prime
+ */
+int is_prime_number(int n)
+{
+	if (n <= 1)
+		return (0);
+	return (is_prime(n, 2));
+}
