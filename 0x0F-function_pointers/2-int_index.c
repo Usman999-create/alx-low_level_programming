91430d5adf97757816b6df2897d6ce53c6da4762
usman999-create <usmanyusu4all88@gmail.com>
2022-07-06 08:57:08 -0700

    1-array_iterator.c

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include "function_pointers.h"
+/**
+ * int_index - function that searches
+ * for an integer
+ *
+ * @array: array with parameters
+ * @size: size of array
+ * @cmp: function to call function
+ *
+ * Return: returns the index of the first element
+ * for which the cmp function does not return 0
+ */
